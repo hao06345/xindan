fdca00dbd5981be0be1bf73eb4fef8b8dafcaf66
hao06345 <2290067033@qq.com>
2019-02-22 14:45:26 +0800

    2.22 14:45

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,28 @@
-var cuijn_gulp = require('gulp');
-function index() {
-	return cuijn_gulp
-		.src('./src/index.html')
-		.pipe(cuijn_gulp.dest('./dist'))
-}
+var gulp = require('gulp');
+var gulpSass = require('gulp-sass');
 
-cuijn_gulp.task('index', index)
+//sass
+function sass() {
+	return gulp
+	.src('./src/style/**/*.scss')
+	.pipe(gulpSass({
+		// outputStyle: 'nested'
+		// outputStyle: 'compact'
+		outputStyle:'expanded'
+		// outputStyle: 'compressed'
+	}).on('error', gulpSass.logError))
+	.pipe(gulp.dest('./dist/css'))
+}
+gulp.task('sass', sass)
+
+gulp.task('watch', function () {
+	return gulp.watch('./src/style/*.scss', sass)
+})
+
+//index
+function index() {
+	return gulp
+		.src('./src/index.html')
+		.pipe(gulp.dest('./dist'))
+}
+gulp.task('index', index)
